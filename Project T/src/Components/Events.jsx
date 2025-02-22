4f47e3ca05decdc9cd2202089e274d9625f44c74
Michael Hrstka <theGeigus@gmail.com>
2025-02-22 15:59:54 -0600

    aaa

--- Project T/src/Components/Events.jsx
+++ Project T/src/Components/Events.jsx
@@ -1,13 +1,13 @@
 import React from 'react'
 import "./Events.css"
 
-const Events = () => {
+const Evt = () => {
     return (
         <>
-        <div className='events-header' style={{flexDirection: 'column'}}>
+        <div className='eh' style={{flexDirection: 'column'}}>
             <img src="src/Assets/luckies_logo.png" alt="logo" className='logo-img' />
-            <h1 className="eventWelcome">Welcome! At Lucky's on third, we dance to the beat of our own pub. call it home, call it a friend, call it the happiest hours of your life. lucky's is your kind of bar. come make memories here.</h1>
-            <img className="weekImg" src="https://static.wixstatic.com/media/263d76_11d4cdc728b34c98961b28bc8ad1bef7~mv2.png/v1/fill/w_1242,h_699,al_c,q_90,usm_0.66_1.00_0.01,enc_avif,quality_auto/Sunday%20(1366%20%C3%97%20769%20px).png"/>
+            <h1 className="eW">Welcome! At Lucky's on third, we dance to the beat of our own pub. call it home, call it a friend, call it the happiest hours of your life. lucky's is your kind of bar. come make memories here.</h1>
+            <img className="wI" src="https://static.wixstatic.com/media/263d76_11d4cdc728b34c98961b28bc8ad1bef7~mv2.png/v1/fill/w_1242,h_699,al_c,q_90,usm_0.66_1.00_0.01,enc_avif,quality_auto/Sunday%20(1366%20%C3%97%20769%20px).png"/>
             <h2>Upcoming</h2>
             <h1>Events</h1>
             <br></br>
@@ -20,4 +20,4 @@ const Events = () => {
         </>
     )
 }
-export default Events
+export default Evt
